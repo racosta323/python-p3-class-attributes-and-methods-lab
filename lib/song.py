8964d94b201a1ac04a262e9fee60e5003650d32e
Rene <racosta323@gmail.com>
2024-02-27 08:59:33 -0600

    updates genre count method

--- lib/song.py
+++ lib/song.py
@@ -10,10 +10,15 @@ class Song:
     def __init__(self, name, artist, genre):
         self.name = name
         self.artist = artist
-        self.genre = genre
-        self.artists.append(artist)
-        self.add_to_genres(genre)
+        # self.genre = genre
+        if self.check_genre(genre):
+            self.genre = genre     
+        else:
+            self.genres.append(genre)
         self.add_song_to_count()
+        self.add_to_genre_count()
+        self.artists.append(artist)
+        
         # self.add_to_genre_count()
 
     @classmethod
@@ -28,15 +33,6 @@ class Song:
     def check_artist(cls,artist):
         return artist in cls.artists
     
-    @classmethod
-    def add_to_genres(cls, genre):
-        if not cls.check_genre(genre):
-            cls.genres.append(genre)
-            ipdb.set_trace()
-            cls.add_to_genre_count()
-        else:
-            ipdb.set_trace()
-            cls.add_to_genre_count()
 
     @classmethod
     def add_to_artists(cls, artist):
@@ -45,21 +41,17 @@ class Song:
 
     @classmethod
     def add_to_genre_count(cls, increment =1):
-        ipdb.set_trace()
         for genre in cls.genres:
-            ipdb.set_trace()
             if genre in cls.genre_count:
-                ipdb.set_trace()
-                cls.genre_count.update({genre: cls.genre_count[genre] + increment})
-                ipdb.set_trace()
+                cls.genre_count[genre] +=1
             else:
-                ipdb.set_trace()
-                cls.genre_count[genre] = increment
-
+                cls.genre_count[genre] = 1
+    
 
 ninety_nine_problems = Song("99 Problems", "Jay-Z", "Rap")
 other_song = Song("99 Problems", "Jay-Z", "Rock")
 another_song = Song("99 Problems", "Jay-Z", "Rock")
+print(Song.genre_count)
 # # print(Song.count)
 # # # Song.add_to_genres("Rap")
 # Song.add_to_genre_count()
